7433978d2336750f87213cb32f6db57e8b1a3285
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-03-25 11:55:42 -0600

    accounts refactor and isLogued middleware

---
+++ accouma_api/app/Http/Middleware/login/isLoguedMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware\login;
+
+use Closure;
+use Response;
+use DB;
+use App\models\usersModel as users;
+
+class isLoguedMiddleware{
+
+
+	public function handle($request, Closure $next){
+		$id = $request->get('id', 0);
+		$token = $request->get('token', '');
+
+		$user = users::where('id', '=', $id)
+			->where('token', '=', $token)
+			->get();
+		if(count($user) == 1){
+			return $next($request);
+		}else{
+			return Response::json([
+				'result' => [
+					'logued' => false
+				],
+				'msg' => 'Not logued'
+			], 200);
+		}
+	}
+
+}
