bfad8287ac0a3b0d19c5cef9d58af774dfb299e7
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-03-28 23:34:13 -0600

    make pagination

--- accouma_api/app/Http/Middleware/basic/paginationMiddleware.php
+++ accouma_api/app/Http/Middleware/basic/paginationMiddleware.php
@@ -4,12 +4,28 @@ namespace App\Http\Middleware\basic;
 
 use Closure;
 use Response;
+use DB;
 
 class paginationMiddleware{
 
 	public function handle($request, Closure $next){
-		$request->attributes->add(['skip' => 0]);
-		$request->attributes->add(['take' => 10]);
+		$segment = $request->segment(3);
+		$page = $request->get('page', 0);
+		$per_page = $request->get('per_page', 0);
+		$q = '';
+		if($page > 0 && $per_page > 0){
+			if($segment == 'users'){
+				$q = DB::table('users')->count();
+			}
+			$tot_pages = ($q - ($q % $per_page)) / $per_page;
+			$tot_pages = ($q % $per_page > 0)? ++$tot_pages : $tot_pages;
+			$tot_pages = ($q <= $per_page)? 1 : $tot_pages;
+			$skip = ($page * $per_page) - $per_page;
+			$request->attributes->add(['skip' => $skip]);
+			$request->attributes->add(['take' => $per_page]);
+			$request->attributes->add(['tot_pages' => $tot_pages]);
+		}
+
 		return $next($request);
 	}
 
