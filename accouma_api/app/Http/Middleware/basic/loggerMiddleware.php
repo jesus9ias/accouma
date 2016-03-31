6756690f6d32089b4fdc3ff6c9391759294d47a4
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-03-30 21:33:26 -0600

    refactor on logger

--- accouma_api/app/Http/Middleware/basic/loggerMiddleware.php
+++ accouma_api/app/Http/Middleware/basic/loggerMiddleware.php
@@ -9,11 +9,10 @@ use App\models\logsModel as Logs;
 class loggerMiddleware{
 
     public function handle($request, Closure $next){
-      $segment = $request->segment(3);
+      $path = $request->path();
       Logs::createLog([
         'user_id' => $request->get('id', 0),
-        'section' => $segment,
-        'action' => '',
+        'path' => $path,
         'date_created' => date("Y-m-d H:i:s")
       ]);
       return $next($request);
