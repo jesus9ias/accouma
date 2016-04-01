9a3334b42ac1d46f79f2e4bda73f7223b3c5e745
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-04-01 00:34:42 -0600

    instaling react material

--- accouma_front/dev/index.jsx
+++ accouma_front/dev/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import RaisedButton from 'material-ui/lib/raised-button';
 
 ReactDOM.render(
-  <h1>Accouma - Front End</h1>,
+  <div>
+    <h1>Accouma - Front End</h1>
+    <RaisedButton label="Default" />
+  </div>,
   document.getElementById('general')
 )
