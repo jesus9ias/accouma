569424ca90a590b77cc402975ac9c18fccfb8cb4
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-04-02 11:11:15 -0600

    Firsth login screen and login styles

--- accouma_front/dev/login/Login.jsx
+++ accouma_front/dev/login/Login.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import {RaisedButton, Card, CardHeader, CardMedia, TextField} from 'material-ui';
+import {InputP} from '../common/reactMaterialProperties'
 
 class Login extends React.Component {
   render() {
-      return (
-        <Card className="login">
+    return (
+      <section className="login">
+        <Card className="login-form">
           <CardHeader title="Accouma - Login" />
           <CardMedia>
-            <TextField className="login-field" fullWidth={true} hintText="User" />
-            <TextField className="login-field" fullWidth={true} hintText="Password" />
+            <TextField className="login-field" style={InputP()} hintText="User" />
+            <TextField className="login-field" style={InputP()} hintText="Password" />
 
-            <RaisedButton label="Login" />
+            <RaisedButton label="Login" fullWidth={false} />
           </CardMedia>
         </Card>
+      </section>
     )
   }
 }
