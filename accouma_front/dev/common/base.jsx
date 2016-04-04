c11d3197aae8469795606e4b1a8af1c39fc92be1
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-04-03 23:52:16 -0500

    sidenav funcgions

--- accouma_front/dev/common/base.jsx
+++ accouma_front/dev/common/base.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
-import {AppBar} from 'material-ui';
+import {AppBar, RaisedButton} from 'material-ui';
+import SideNav from '../common/sideNav'
 
 class Base extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {isSideNavOpen: false};
+  }
+
+  toggleSideNav(){
+    alert('ok');
+    //this.setState({isSideNavOpen: !this.state.isSideNavOpen});
+  }
+
+  handleTouchTap(){
+    alert('yes');
+  }
+
   render() {
     return (
       <section className={this.props.section}>
-        <AppBar title="Accouma" iconClassNameRight="muidocs-icon-navigation-expand-more"/>
+        <AppBar title="Accouma" onLeftIconButtonTouchTap={this.toggleSideNav.bind(this)} onTitleTouchTap={this.handleTouchTap} iconClassNameRight="muidocs-icon-navigation-expand-more"></AppBar>
+        <SideNav isSideNavOpen={this.state.isSideNavOpen} toggleSideNav={this.toggleSideNav.bind(this)} />
         <section className="content">
           {this.props.children}
         </section>
