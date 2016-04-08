0a19f4f4dbb393c5f9c52776f325fc3282585268
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-04-07 23:45:04 -0500

    changin components with Belle

--- accouma_front/dev/common/base.jsx
+++ accouma_front/dev/common/base.jsx
@@ -1,31 +1,14 @@
 import React from 'react'
-import {AppBar, IconButton, RaisedButton, FlatButton, NavigationClose} from 'material-ui';
-import SideNav from '../common/sideNav'
+import {Card, TextInput, Button} from 'Belle'
 
 class Base extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {isSideNavOpen: false};
-  }
-
-  toggleSideNav(){
-    this.setState({isSideNavOpen: !this.state.isSideNavOpen});
-  }
-
-  handleTouchTap(){
-    alert('onTouchTap triggered on the title component');
-  }
 
   render() {
     return (
       <section className={this.props.section}>
-        <AppBar title="Accouma"
-          iconElementRight={<FlatButton onClick={this.toggleSideNav.bind(this)} label="SideNav" />} >
-
-        </AppBar>
-        <IconButton iconClassName="muidocs-icon-navigation-expand-more" onClick={this.toggleSideNav.bind(this)} />
-        <SideNav isSideNavOpen={this.state.isSideNavOpen} toggleSideNav={this.toggleSideNav.bind(this)} />
+        <Card>
+          <h1>Accouma</h1>
+        </Card>
         <section className="content">
           {this.props.children}
         </section>
