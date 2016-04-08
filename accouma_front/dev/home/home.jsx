734f9d81ba3120cac804a0de0fb291055282eb93
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-04-07 23:45:04 -0500

    changin components with Belle

--- accouma_front/dev/home/home.jsx
+++ accouma_front/dev/home/home.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import {RaisedButton, Card, CardHeader, CardMedia, TextField, AppBar} from 'material-ui';
-import {InputP} from '../common/reactMaterialProperties'
 import Base from '../common/base'
 
 class Home extends React.Component {
