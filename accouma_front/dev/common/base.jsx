b4a85ef89648d222e101f0d87ea68bc373fcbb06
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-04-08 22:12:09 -0500

    apply some basic styles

--- accouma_front/dev/common/base.jsx
+++ accouma_front/dev/common/base.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Card, TextInput, Button} from 'Belle'
+import {CardP} from '../common/styleProperties'
 
 class Base extends React.Component {
 
   render() {
     return (
       <section className={this.props.section}>
-        <Card>
+        <Card style={CardP()}>
           <h1>Accouma</h1>
         </Card>
         <section className="content">
--- accouma_front/dev/login/Login.jsx
+++ accouma_front/dev/login/Login.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import {Card, TextInput, Button} from 'Belle'
-import {InputP} from '../common/reactMaterialProperties'
+import {InputP, CardP} from '../common/styleProperties'
 
 class Login extends React.Component {
+
+  ok(){
+    alert('ok');
+  }
+
   render() {
     return (
       <section className="login">
-        <Card className="login-form">
-          <h2>Accouma - Login</h2>
-          <TextInput className="login-field" placeholder="User" />
-          <TextInput className="login-field" placeholder="Password" />
-          <Button primary>Login</Button>
+        <Card className="login-form" style={CardP()}>
+          <h2 className="login-title">Accouma - Login</h2>
+          <TextInput className="login-field" style={InputP()} placeholder="User" />
+          <TextInput className="login-field" style={InputP()} placeholder="Password" />
+          <Button primary submit onClick={this.ok}>Login</Button>
         </Card>
       </section>
     )
