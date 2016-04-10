a0446e5d55cd2253ecdfc073bb4095ebd42bbe94
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-04-10 11:10:02 -0500

    handlesr for login inputs

--- accouma_front/dev/home/home.jsx
+++ accouma_front/dev/home/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Base from '../common/base'
+import {Link} from 'react-router'
 
 class Home extends React.Component {
   render() {
     return (
       <Base section="home" >
         <p>Hi!</p>
+        <Link to={'/users'}>Users</Link>
       </Base>
     )
   }
