c163b781bc08f64633de9ccf2372238871ddc5f0
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-04-09 23:33:22 -0500

    creating some components

--- accouma_front/dev/index.jsx
+++ accouma_front/dev/index.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Login from './login/login'
 import Home from './home/home'
+import Users from './users/users'
+import Accounts from './accounts/accounts'
+import Registers from './registers/registers'
 
 import { Router, Route, Link, browserHistory } from 'react-router'
 
@@ -9,6 +12,9 @@ ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={Home}/>
     <Route path="/login" component={Login} />
+    <Route path="/users" component={Users} />
+    <Route path="/accounts" component={Accounts} />
+    <Route path="/registers" component={Registers} />
     <Route path="*" component={Home} />
   </Router>,
   document.getElementById('general')
