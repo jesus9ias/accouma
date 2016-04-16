925128c169f1bc54780b6007a18e8d0580592971
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-04-15 23:00:19 -0500

    preparing react icons

--- accouma_front/dev/home/home.jsx
+++ accouma_front/dev/home/home.jsx
@@ -3,6 +3,7 @@ import {Card, TextInput, Button} from 'Belle'
 import Base from '../common/base'
 import {InputP, CardP} from '../common/styleProperties'
 import {Link} from 'react-router'
+import MdArrowForward from 'react-icons/lib/md/arrow-forward'
 
 class Home extends React.Component {
   render() {
@@ -12,15 +13,15 @@ class Home extends React.Component {
       <Base section="home" >
         <Card className="home-block" style={CardS}>
           <h2 className="home-blockTitle">Users</h2>
-          <Link className="home-blockLink" to={'/users'}>Users</Link>
+          <Link className="home-blockLink" to={'/users'}><MdArrowForward /></Link>
         </Card>
         <Card className="home-block" style={CardS}>
           <h2 className="home-blockTitle">Registers</h2>
-          <Link className="home-blockLink" to={'/registers'}>Registers</Link>
+          <Link className="home-blockLink" to={'/registers'}><MdArrowForward /></Link>
         </Card>
         <Card className="home-block" style={CardS}>
           <h2 className="home-blockTitle">Accounts</h2>
-          <Link className="home-blockLink" to={'/accounts'}>Accounts</Link>
+          <Link className="home-blockLink" to={'/accounts'}><MdArrowForward /></Link>
         </Card>
       </Base>
     )
