09f20f34e863587be04b15b44810dbd7f4aa9e60
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-05-01 11:08:18 -0500

    setting env var for switching auth

--- accouma_api/app/Http/Middleware/auth/isAdminMiddleware.php
+++ accouma_api/app/Http/Middleware/auth/isAdminMiddleware.php
@@ -9,17 +9,22 @@ use App\models\usersRolesModel as usersRoles;
 class isAdminMiddleware{
 
 	public function handle($request, Closure $next){
+		$use_auth = env('USE_AUTH',true);
 		$id = $request->get('id', 0);
-		$roles = usersRoles::getRoleByUser($id, 'admin', 2);
-		if(count($roles) >= 1){
-			return $next($request);
+		if($use_auth == true){
+			$roles = usersRoles::getRoleByUser($id, 'admin', 2);
+			if(count($roles) >= 1){
+				return $next($request);
+			}else{
+				return Response::json([
+					'result' => [
+						'has' => false
+					],
+					'msg' => 'Has Not Admin Role'
+				], 200);
+			}
 		}else{
-			return Response::json([
-				'result' => [
-					'has' => false
-				],
-				'msg' => 'Has Not Admin Role'
-			], 200);
+			return $next($request);
 		}
 	}
 
--- accouma_api/app/Http/Middleware/auth/isLoguedMiddleware.php
+++ accouma_api/app/Http/Middleware/auth/isLoguedMiddleware.php
@@ -10,21 +10,26 @@ class isLoguedMiddleware{
 
 
 	public function handle($request, Closure $next){
+		$use_auth = env('USE_AUTH',true);
 		$id = $request->get('id', 0);
 		$token = $request->get('token', '');
 
-		$user = users::where('id', '=', $id)
-			->where('token', '=', $token)
-			->get();
-		if(count($user) == 1){
-			return $next($request);
+		if($use_auth == true){
+			$user = users::where('id', '=', $id)
+				->where('token', '=', $token)
+				->get();
+			if(count($user) == 1){
+				return $next($request);
+			}else{
+				return Response::json([
+					'result' => [
+						'logued' => false
+					],
+					'msg' => 'Not logued'
+				], 200);
+			}
 		}else{
-			return Response::json([
-				'result' => [
-					'logued' => false
-				],
-				'msg' => 'Not logued'
-			], 200);
+			return $next($request);
 		}
 	}
 
