063a700d2d44dfb9165b72d36a23ab600db87d63
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-07-07 21:56:43 -0500

    sass linting and some refactor

--- accouma_front/gulpfile.js
+++ accouma_front/gulpfile.js
@@ -1,20 +1,27 @@
-var gulp = require('gulp')
-var webserver = require('gulp-webserver')
-var sass = require('gulp-sass')
-var browserify = require('browserify')
-var babelify = require('babelify')
-var source = require('vinyl-source-stream')
-var minify = require('gulp-minify-css')
-var php = require('gulp-connect-php')
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var sass = require('gulp-sass');
+var scsslint = require('gulp-scsslint');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var minify = require('gulp-minify-css');
+var php = require('gulp-connect-php');
 
 gulp.task('sass', function() {
-  gulp.src('./dev/style.scss')
+  gulp.src(['./dev/style.scss'])
     .pipe(sass({
       'include css': true,
     }))
     .pipe(minify())
     .pipe(gulp.dest('./app/build/css/'))
 })
+
+gulp.task('sassLint', function() {
+  gulp.src(['dev/**/*.scss', '!./dev/common/vendor/**/*.scss'])
+    .pipe(scsslint())
+    .pipe(scsslint.reporter());
+});
 
 gulp.task('build', function() {
   browserify({
